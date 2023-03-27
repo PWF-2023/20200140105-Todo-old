4f808558edb247ac25fff675ba565818fd1f1ef6
Salwasalsabilaa <azhar.salwa.ft20@mail.umy.ac.id>
2023-03-27 19:43:48 +0700

    env, mvc, route, relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
+    //Relasi One to Many
+    public function todos()
+    {
+        return $this->hasMany(Todo::class);
+    }
 }
